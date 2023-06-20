6b36bcb0e44b2e6e57a70e24c1b1b1ed6950f18d
bwrosen20 <bwrosen20@gmail.com>
2023-06-20 11:04:40 -0400

    commit tree d0fc55df93341cb63b8fc8748cf93c75752952a5

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end 
+
+    def show
+        current_user = User.find(session[:user_id])
+        render json: current_user
+    rescue ActiveRecord::RecordNotFound
+        render json: {error: "Please login or signup"}, status: :unauthorized
+    end 
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
